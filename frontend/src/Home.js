6d3bbeb8e3b4dd4e07f5fff394b7aaaea412b476
SIAILYA <sia.ilya.dev@gmail.com>
2022-01-10 07:46:35 +0300

    Разобрался с Tailwind, обновил WebStorm, удалил все лишнее

---
+++ frontend/src/Home.js
@@ -0,0 +1,11 @@
+import "./styles/main.css"
+
+function Home() {
+    return (
+        <div className="text-cyan-500 text-center">
+            Hello world! Its a MemesGames!
+        </div>
+    );
+}
+
+export default Home;
